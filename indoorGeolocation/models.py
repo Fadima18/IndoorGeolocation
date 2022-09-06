cd40cfcf41f53603cad692499e5ce02fb882d7fb
Fadima18 <dnfatou@ept.sn>
2022-09-06 17:10:10 +0000

    unit_test

--- indoorGeolocation/models.py
+++ indoorGeolocation/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Device(models.Model):
     device_id = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 
 class Position(models.Model):
@@ -15,17 +17,24 @@ class Position(models.Model):
     instant = models.DateTimeField(auto_now=True)
     device = models.ForeignKey(
         Device, related_name='device_positions', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.device.name
 
 
 class Node(models.Model):
     device = models.OneToOneField(
         Device, on_delete=models.PROTECT, blank=True, null=True)
+    def __str__(self):
+        return self.device.name
 
 
 class Person(Node):
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
-
+    def __str__(self):
+        return self.firstName
 
 class Material(Node):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
