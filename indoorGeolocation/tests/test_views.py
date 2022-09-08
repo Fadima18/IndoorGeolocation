3b905006862d3770a5bb64fc116eae442ac38dcd
Massamba Sene <smmassamba@ept.sn>
2022-09-08 15:44:23 +0000

    Modification des tests

--- indoorGeolocation/tests/test_views.py
+++ indoorGeolocation/tests/test_views.py
@@ -9,10 +9,11 @@ class TestViews(TestCase):
         self.track_material_url = reverse("track_material")
         self.track_person_url = reverse("track_person")
         self.analytics_url = reverse("analytics")
+        self.analytics_template = "indoorGeolocation/analytics.html"
         self.track_specific_material_url = '/indoor/track_specific_material?name=Tv'
         self.track_specific_person_url = "/indoor/track_specific_person/noone?name=Mass"
         self.track_mass_url = "/indoor/track_specific_person/Mass"
-        
+
         self.device = Device.objects.create(
             device_id="eui-193991",
             name="device1"
@@ -20,11 +21,11 @@ class TestViews(TestCase):
 
     def testTrackMaterialShouldReturnTrackMaterialPage(self):
         Material.objects.create(
-            device = self.device,
-            name = "Test"
+            device=self.device,
+            name="Test"
         )
         Position.objects.create(
-            device = self.device,
+            device=self.device,
             x=14.795036483073138,
             y=-16.9649700075388
         )
@@ -52,23 +53,25 @@ class TestViews(TestCase):
         reponse = self.client.get(self.track_specific_material_url)
 
         self.assertEquals(reponse.status_code, 200)
-        self.assertTemplateUsed("indoorGeolocation/specific_object_tracking.html")
+        self.assertTemplateUsed(
+            "indoorGeolocation/specific_object_tracking.html")
 
     def testAnalyticsShouldReturnAnalyticsPage(self):
         reponse = self.client.get(self.analytics_url)
 
         self.assertEquals(reponse.status_code, 200)
         self.assertTemplateUsed("indoorGeolocation/analytics.html")
-        
+
     def testViewRoomAnalyticsShouldReturnAnalyticsPage(self):
         room = "Chambre8"
         room_coordinates = [14.79498461743184, -16.96512758731842]
-        Position.objects.create(x=room_coordinates[0], y=room_coordinates[1], device=self.device)
-        
+        Position.objects.create(
+            x=room_coordinates[0], y=room_coordinates[1], device=self.device)
+
         response = self.client.post(self.analytics_url, {'room': room})
-        
+
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed("indoorGeolocation/analytics.html")
+        self.assertTemplateUsed(self.analytics_template)
 
     def testSpecificPersonTrackingShouldReturnSpecificPersonTrackingPage(self):
         Person.objects.create(
@@ -78,7 +81,8 @@ class TestViews(TestCase):
         )
         reponse = self.client.get(self.track_specific_person_url)
         self.assertEquals(reponse.status_code, 200)
-        self.assertTemplateUsed("indoorGeolocation/specific_person_tracking.html")
+        self.assertTemplateUsed(
+            "indoorGeolocation/specific_person_tracking.html")
 
     def testViewAnalyticsContext(self):
         reponse = self.client.get(self.analytics_url)
@@ -89,17 +93,18 @@ class TestViews(TestCase):
         self.assertIsInstance(context['days_labels'], list)
         self.assertIsInstance(context['days_data'], list)
         self.assertIsInstance(context['today_visits'], int)
-        
+
     def testViewRoomAnalyticsContext(self):
         room = "Chambre8"
         room_coordinates = [14.79498461743184, -16.96512758731842]
-        Position.objects.create(x=room_coordinates[0], y=room_coordinates[1], device=self.device)
-        
+        Position.objects.create(
+            x=room_coordinates[0], y=room_coordinates[1], device=self.device)
+
         response = self.client.post(self.analytics_url, {'room': room})
         context = response.context
-        
+
         self.assertNotEqual(context, {})
-        self.assertTemplateUsed("indoorGeolocation/analytics.html")
+        self.assertTemplateUsed(self.analytics_template)
 
     def testPersonTrackingContext(self):
         reponse = self.client.get(self.track_person_url)
@@ -120,7 +125,7 @@ class TestViews(TestCase):
         context = reponse.context
 
         self.assertIsInstance(context['map'], str)
-        
+
     def testLiveTrackingShouldNotReturnEmptyResponse(self):
         Person.objects.create(
             device=self.device,
@@ -128,18 +133,18 @@ class TestViews(TestCase):
             lastName="Sene"
         )
         response = self.client.post(self.track_person_url, {'action': 'post'})
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(response.context, {})
-        
+
     def testLiveTrackingSpecificPersonShouldNotReturnEmptyResponse(self):
         Person.objects.create(
             device=self.device,
             firstName="Mass",
             lastName="Sene"
         )
-        
+
         response = self.client.post(self.track_mass_url, {'action': 'post'})
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(response.context, {})
