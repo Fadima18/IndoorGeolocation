088fbb539d7de7d0c87ecb713b1968819764e94e
Massamba Sene <smmassamba@ept.sn>
2022-09-10 11:22:15 +0000

    nc

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 
 setup(name='Geolocation',
       version='1.0',
-      packages=find_packages())
+      packages=find_packages(),
+      scripts=['manage.py'])
