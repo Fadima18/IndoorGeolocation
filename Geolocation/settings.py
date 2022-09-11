8ec6ca7c4db7d9c34921ebfe1429f26c847aaa6d
kingsene19 <87582441+kingsene19@users.noreply.github.com>
2022-09-11 00:23:07 +0000

    Update settings.py

--- Geolocation/settings.py
+++ Geolocation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6oubom3#)=j)6e!*1h84w%3js71iipxsb%q7k=x2h&$wg0^@7s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
