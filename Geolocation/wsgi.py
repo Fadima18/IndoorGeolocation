045be7d960d0e069fd6c39140d5b1055f50c38c8
kingsene19 <87582441+kingsene19@users.noreply.github.com>
2022-09-11 12:47:58 +0000

    Update wsgi.py

--- Geolocation/wsgi.py
+++ Geolocation/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Geolocation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Geolocation.settings.production')
 
 application = get_wsgi_application()
